1736c352fa7fd97fff4d4fe321daf3d9592cb06c
bnuri00 <bnurik104@gmail.com>
2023-10-18 14:23:42 +0900

    feat: 공통 Enum class 및 abstract Converter 추가, 공통 엔티티 추가
    - EnableJpaAuditing 이용한 공통 엔티티 (BaseDateTime)
    
    #9

--- src/main/java/com/moa/meeting/BeMeetingApplication.java
+++ src/main/java/com/moa/meeting/BeMeetingApplication.java
@@ -3,8 +3,10 @@ package com.moa.meeting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BeMeetingApplication {
 
---
+++ src/main/java/com/moa/meeting/common/BaseEnum.java
@@ -0,0 +1,9 @@
+package com.moa.meeting.common;
+
+
+public interface BaseEnum<T, K> {
+
+	T getCode();
+	K getTitle();
+
+}
