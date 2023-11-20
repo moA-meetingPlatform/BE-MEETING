338350d9b0510a44d93cce634634d188d496b758
bnuri00 <bnurik104@gmail.com>
2023-11-20 12:15:27 +0900

    fix: json serialize 에러 해결 시도 - dto에 @JsonSerialize(using = ToStringSerializer.class) 달기
    #22

--- src/main/java/com/moa/meeting/dto/CategoryMeetingCreateDto.java
+++ src/main/java/com/moa/meeting/dto/CategoryMeetingCreateDto.java
@@ -1,6 +1,8 @@
 package com.moa.meeting.dto;
 
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.moa.meeting.domain.enums.CanParticipateGender;
 import lombok.*;
 
@@ -12,13 +14,28 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 // 모임 생성시 db에 저장되는 정보
-public class CategoryMeetingGetDto {
+public class CategoryMeetingCreateDto {
+
+	@JsonSerialize(using = ToStringSerializer.class)
 	private Long categoryMeetingId; //카테고리 모임 리스트 Id
+
+	@JsonSerialize(using = ToStringSerializer.class)
 	private Integer topCategoryId;   //상위카테고리 Id
+
+	@JsonSerialize(using = ToStringSerializer.class)
 	private Integer subCategoryId;  //하위 카테고리 Id
+
+	@JsonSerialize(using = ToStringSerializer.class)
 	private Integer maxAge;    //나이 상한선
+
+	@JsonSerialize(using = ToStringSerializer.class)
 	private Integer minAge;    //나이 하한선
+
 	private CanParticipateGender participateGender; //참여가능한 성별
+
 	private String participateCompanies;  //참여가능한 기업 리스트
+
+	@JsonSerialize(using = ToStringSerializer.class)
 	private Boolean enable;     //모임 종료, 모임 취소, 모임 삭제시 : 0으로 바꾸기
+
 }
